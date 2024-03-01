3a500d917b015a85a2e81610d86b8aa516dde2e1
borisgil1 <borisgil1@hotmail.com>
2024-02-29 23:51:45 -0500

    testing

--- main.js
+++ main.js
@@ -1,8 +1,56 @@
-let mascota = "Melon"
-let mascotaEdad = 5;
+class ProductManager {
+    static lastId = 0;
+    constructor() {
+        this.products = [];
+    }
 
-console.log("Nuestro chandoso " + mascota + " tiene " + mascotaEdad + " años");
+    addProduct(title, description, price, img, code, stock) {
+        if (!title || !description || !price || !img || !code || !stock) {
+            console.log("Favor llenar todos los campos");
+            return;
+        }
 
-console.log(`Nuestro chandoso ${mascota} tiene ${mascotaEdad + 10} años`)
+        if (this.products.some(item => item.code === code)) {
+            console.log("El código debe ser único");
+            return;
+        }
 
-    
+        const newProduct = {
+            id: ++ProductManager.lastId,
+            title,
+            description,
+            price,
+            img,
+            code,
+            stock,
+        };
+
+        this.products.push(newProduct);
+    }
+
+    getProduct() {
+        return this.products;
+    }
+
+    getProductById(id) {
+        const product = this.products.find(item => item.id === id);
+        if (!product) {
+            console.log("Producto no encontrado");
+        } else {
+            console.log("Producto encontrado:", product);
+        }
+    }
+}
+
+//Testing
+const manager = new ProductManager();
+console.log(manager.getProduct());
+manager.addProduct("Ps4", "Consola", 35000000, "Ps4.jpg", "001", 35);
+
+manager.addProduct("Ps5", "Consola", 4000000, "Ps5.jpg", "002", 14);
+
+manager.addProduct("Ps5", "Consola", 4000000, "Ps5.jpg", "002", 14);
+
+console.log(manager.getProduct());
+console.log(manager.getProductById(3));
+
