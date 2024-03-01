7f9046c87749684c7ee617a3b61ef4802236ead1
borisgil1 <borisgil1@hotmail.com>
2024-02-29 23:27:45 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+let mascota = "Melon"
+let mascotaEdad = 5;
+
+console.log("Nuestro chandoso " + mascota + " tiene " + mascotaEdad + " años");
+
+console.log(`Nuestro chandoso ${mascota} tiene ${mascotaEdad + 10} años`)
+
+    
